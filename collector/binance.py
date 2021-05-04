31c1fbb010310102fee49951b675d44d50a75c7a
Gustavo Barrios <gustavoantonio51@gmail.com>
2021-05-04 23:18:50 +0900

    WIP: Binance candlestick data, testing api requests and fixing type hints

--- collector/binance.py
+++ collector/binance.py
@@ -3,16 +3,22 @@ import json
 import time
 from requests.exceptions import HTTPError
 import pandas as pd
-from typing import Any, List, Dict, Callable, TypeVar
+from typing import Any, Union, Callable
 import re
 
 class Binance:
-
-    T = TypeVar('T', bound=Callable[..., Any])
     
-    def __init__(self, api_key:str,
-                 symbol:str, interval:str,
-                 startTime:str, endTime:str, limit:int = 500) -> None:
+    api_key: str
+    symbol: str
+    interval: str
+    startTime: int
+    endTime: int
+    limit: int
+
+
+    def __init__(self, api_key,
+                 symbol, interval,
+                 startTime, endTime, limit = 500) -> None:
         self.symbol = symbol
         self.interval = interval
         self.startTime = startTime
@@ -20,7 +26,7 @@ class Binance:
         self.limit = limit
         self.api_key = api_key
     
-    def api_handler(self, url:str, timer:int, retries:int=5) -> T:
+    def api_handler(self, url:str, timer:int, retries:int=5) -> Union[Callable[Any], requests.models.Response]:
         try:
             r = requests.get(url, headers={'X-MBX-APIKEY':self.api_key})
             if r.status_code != 200:
@@ -44,9 +50,8 @@ class Binance:
                 print('\n{} Binance probably down...'.format(r.status_code))
                 time.sleep(3600)
                 return self.api_handler(url, timer)
-
-
-
+        finally:
+            return r
     
     def kline(self) -> pd.DataFrame:
         url = 'https://api.binance.com/api/v3/klines?symbol='+self.symbol+'&interval='+self.interval
