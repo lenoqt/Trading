8ab973082215cfe3f0316957fdc8ddef2068bd47
Gustavo Barrios <gustavoantonio51@gmail.com>
2021-09-20 17:35:10 +0900

    some fixes, still refactoring everything

--- trading/utils/endpoints.py
+++ trading/utils/endpoints.py
@@ -1,25 +1,51 @@
 # type: ignore
 from enum import Enum
 
+
 __all__ = ['BinanceEndpoint']
 
-class BinanceEndpoint(Enum):
-	BASE_URL = 'https://api.binance.com'
-#Endpoints (GET)
-	TEST = '/api/v3/ping'
-	TIME = '/api/v3/time'
-	INFO = '/api/v3/exchangeInfo'
-	ORDERBOOK = '/api/v3/depth'
-	TRADES = '/api/v3/trades'
-	HISTORICAL = '/api/v3/historicalTrades'
-	AGGREGATED = '/api/v3/aggTrades'
-	KLINES = '/api/v3/klines'
-	AVGPRICE = '/api/v3/avgPrice'
-	DAYSTATS = '/api/v3/ticker/24hr'
-	SYMBOLTPRICE = '/api/v3/ticker/price'
-	SYMBOOKT = '/api/v3/ticker/bookTicker'
+class BinanceEndpoint(str, Enum):
 
-#Intervals for public API
-	INTERVALS = ['1m', '3m', '5m', '15m', '30m',
-				 '1h', '2h', '4h', '6h', '8h', '12h',
-				 '1d', '3d', '1w', '1M']
+    BASE_URL = 'https://api.binance.com'
+    # Endpoints (GET) 
+    TEST = '/api/v3/ping' 
+    TIME = '/api/v3/time' 
+    INFO = '/api/v3/exchangeInfo' 
+    ORDERBOOK = '/api/v3/depth' 
+    TRADES = '/api/v3/trades' 
+    HISTORICAL = '/api/v3/historicalTrades'
+    AGGREGATED = '/api/v3/aggTrades'
+    KLINES = '/api/v3/klines'
+    AVGPRICE = '/api/v3/avgPrice'
+    DAYSTATS = '/api/v3/ticker/24hr'
+    SYMBOLTPRICE = '/api/v3/ticker/price'
+    SYMBOOKT = '/api/v3/ticker/bookTicker'
+    # FQDNs
+    KLINE_FQDN = BASE_URL + KLINES + '?symbol=%s&interval=%s'
+    TEST_FQDN = BASE_URL + TEST 
+    TIME_FQDN = BASE_URL + TIME 
+    INFO_FQDN = BASE_URL + INFO 
+    ORDERBOOK_FQDN = BASE_URL + ORDERBOOK + 'symbol=%s&limit=%s' 
+    HISTORICAL_FQDN = BASE_URL + HISTORICAL + '?symbol=%s&limit=%s' 
+    TRADES_FQDN = BASE_URL + TRADES + '?symbol=%s&limit=%s' 
+    # Intervals for public API 
+    
+    _1MIN = '1m'
+    _3MIN = '3m'
+    _5MIN = '5m'
+    _15MIN = '15m'
+    _30MIN = '30m'
+    _1H = '1h'
+    _2H = '2h'
+    _4H = '4h'
+    _6H = '6h'
+    _8H = '8h'
+    _12H = '12h'
+    _1D = '1d'
+    _3D = '3d'
+    _1W = '1w'
+    _1M = '1M'
+    
+    def describe(self):
+        return self.name, self.value
+
--- trading/utils/handlers.py
+++ trading/utils/handlers.py
@@ -1,8 +1,11 @@
-import requests
-import json
-from requests.exceptions import HTTPError
-from typing import Dict, Any, Union
 from time import time
+from typing import Any, Dict, Union
+
+import requests
+from requests.exceptions import HTTPError
+
+from mysql.connector.errors import DatabaseError, PoolError
+from mysql.connector.pooling import MySQLConnectionPool
 
 __all__ = ['api_handler']
 
@@ -14,7 +17,6 @@ def api_handler(url: str,
     try:
         r = requests.get(url, headers={'X-MBX-APIKEY': api_key})
         r = r.json()
-        r = json.loads(r)
         if r.status_code != 200:
             raise HTTPError('Not expected API response')
     except HTTPError as err:
@@ -38,3 +40,53 @@ def api_handler(url: str,
             return api_handler(url, api_key, timer)
     finally:
         return r
+
+class DBHandler:
+
+    db_host: str
+    db_port: int
+    db_username: str
+    db_password: str
+    db_name: str
+    pool_size: int
+    pool_name: str
+    connected: bool
+
+    def __init__(self, 
+                 db_host, db_port,
+                 db_username, db_password,
+                 db_name, pool_name,
+                 pool_size) -> None:
+        self.db_host = db_host
+        self.db_port = db_port
+        self.db_username = db_username
+        self.db_password = db_password
+        self.db_name = db_name
+        self.pool_name = pool_name
+        self.pool_size = pool_size
+        self.connected = False
+
+    def __enter__(self):
+        while not self.connected:
+            try:
+                connection_pool = MySQLConnectionPool(pool_name=self.pool_name,
+                                                      pool_size=self.pool_size,
+                                                      database=self.db_name,
+                                                      user=self.db_username,
+                                                      password=self.db_password,
+                                                      host=self.db_host,
+                                                      port=self.db_port,
+                                                      pool_reset_session=False) 
+                self.connection = connection_pool.get_connection()
+                if self.connection.is_connected():
+                    self.connected = True
+                    print('Connected to %s' % self.connection.get_server_info()) 
+                    return self.connection
+            except PoolError as err:
+                print('Connection failed due %s' % err)
+            except DatabaseError as err:
+                print("Encountered error while operating DB %s" % err)
+
+    def __exit__(self, type, value, traceback):
+        self.connection.close() 
+
