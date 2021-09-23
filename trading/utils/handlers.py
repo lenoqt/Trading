5a9918e2f67e95549414345997f45a6a9a9f743f
Gustavo Barrios <gustavoantonio51@gmail.com>
2021-09-23 21:53:21 +0900

    changed handlers to send signed requests, TODO: testing and fix type hints

--- trading/utils/handlers.py
+++ trading/utils/handlers.py
@@ -1,45 +1,62 @@
+from requests import request, Response
+import hashlib
+import hmac
+import base64
+import re
+from typing import Any, AnyStr, Dict, Literal, Union, overload
 from time import time
-from typing import Any, Dict, Union
-
-import requests
 from requests.exceptions import HTTPError
-
 from mysql.connector.errors import DatabaseError, PoolError
 from mysql.connector.pooling import MySQLConnectionPool
 
-__all__ = ['api_handler']
+__all__ = ['api_handler', 'DBHandler', 'signature']
 
-def api_handler(url: str,
-                api_key: str, 
+def api_handler(method: str,
+                endpoint: str,
+                api_key: str,
+                secret: str,
                 timer:int=5, 
-                retries:int=5) -> Union[requests.Response, Any, Dict]:
+                retries:int=5) -> Union[Response, Any, Dict]:
     r = {}
     try:
-        r = requests.get(url, headers={'X-MBX-APIKEY': api_key})
+        message = re.search(r'\?([\s\S]*)$', endpoint)
+        sig = signature(secret, message.group(1)) # type: ignore
+        endpoint = endpoint + f"&signature={sig}"
+        r = request(method, endpoint, headers={'X-MBX-APIKEY': api_key})
         r = r.json()
         if r.status_code != 200:
-            raise HTTPError('Not expected API response')
+            r.raise_for_status()
     except HTTPError as err:
         if r.status_code == 429 and retries != 0: # type: ignore
             print('\n\r{} API Response: {}... {}secs'.format(err, r.status_code, round(timer, 2))) # type: ignore
             time.sleep(timer)
             timer += 5
             retries -= 1
-            return api_handler(url, api_key, timer, retries)
+            return api_handler(method, endpoint, api_key, secret,  timer, retries)
         elif r.status_code == 403: # type: ignore
             print('\n{} Sleeping... '.format(r.status_code)) # type: ignore
-            return api_handler(url, api_key, timer)
+            return api_handler(method, endpoint, api_key, secret,  timer)
         elif r.status_code == 418: # type: ignore
             print('\n{} API Key Banned!')
             time.sleep(3600)
-            return api_handler(url,api_key, timer)
+            return api_handler(method, endpoint,api_key, secret,  timer)
         # 5xx: Server side errors
         elif str(r.status_code).startswith('5'): # type: ignore 
             print('\n{} Binance probably down...'.format(r.status_code)) # type: ignore
             time.sleep(3600)
-            return api_handler(url, api_key, timer)
+            return api_handler(method, endpoint, api_key, secret, timer)
     finally:
         return r
+
+def signature(secret: Union[bytes, str], 
+              message: Union[bytes, str],
+              encoding: str = 'utf-8') -> bytes:
+    # TODO: Fix typing on the whole file 
+    secret = bytes(secret, encoding) # type: ignore
+    message = bytes(message, encoding) # type: ignore
+    hashed = hmac.new(secret, message, hashlib.sha256)
+    hashed.hexdigest()
+    return base64.b64encode(hash.digest())
 
 class DBHandler:
 
