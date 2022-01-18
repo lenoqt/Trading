bfa3e8f17e4b6e65296e2ce20ddaff50952172f0
Gustavo Barrios <gustavoantonio51@gmail.com>
2022-01-18 12:14:14 +0900

    WIP: writing tests for each public endpoint

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,15 @@
-import trading.binance as bi 
-import trading.utils as utils 
-import pytest 
+import trading.binance as bi
+import trading.utils as utils
+import pytest
 
-@pytest.fixtures(sc)
 
+@pytest.fixture(scope="session")
+def binance():
+    interval = utils.BinanceIntervals.INTERVAL_1D
+    binance = bi.Binance(
+        symbol="BTCUSDT",
+        start_time="2021-12-01 00:00:00.000000",
+        end_time="2022-01-01 00:00:00.000000",
+        interval=interval,
+    )
+    return binance
--- tests/test_binance.py
+++ tests/test_binance.py
@@ -1,2 +1,12 @@
-import trading.binance as bi
-import trading.utils as utils
+import pytest
+from trading.utils import BinanceEndpoints
+
+
+def test_object(binance):
+    assert binance is not None
+
+
+def test_kline(binance):
+    kline_data = binance.public_endpoints(BinanceEndpoints.KLINE, "GET")
+    assert isinstance(kline_data, dict)
+    assert len(kline_data) == 12
